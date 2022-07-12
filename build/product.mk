b3cf0963c8fe692185a8cdd1176b71222984b5b0
EnesSastim <sastimenes@gmail.com>
2022-07-12 18:55:09 +0000

    Revert "signing: Configure test<->release key migration"
    This reverts commit 8f5edc7684f4e7f12102a6e2837956505bf991da.

--- build/product.mk
+++ build/product.mk
@@ -20,7 +20,6 @@ $(call inherit-product, vendor/proton/fonts/fonts.mk)
 $(call inherit-product, vendor/proton/overlay/overlay.mk)
 $(call inherit-product, vendor/proton/telephony/telephony.mk)
 $(call inherit-product, vendor/proton/audio/audio.mk)
-$(call inherit-product-if-exists, vendor/proton/signing/dev.mk)
 
 # Flatten APEXs for performance
 OVERRIDE_TARGET_FLATTEN_APEX := true

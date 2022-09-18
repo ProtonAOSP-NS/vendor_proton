838a9443b0ee69d1ae89efcee1b7770a34611d82
EnesSastim <sastimenes@gmail.com>
2022-09-18 04:16:46 +0000

    proton: edit bacon target

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -5,14 +5,14 @@ CUSTOM_ROM_TARGET_PACKAGE := $(PRODUCT_OUT)/ProtonAOSP-$(CUSTOM_ROM_VERSION)-$(P
 
 
 .PHONY: bacon signed
-bacon: otapackage
+bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(CUSTOM_ROM_TARGET_PACKAGE)
-	@echo -e "zip: "$(CUSTOM_ROM_TARGET_PACKAGE)
-	@echo -e "size:`ls -lah $(CUSTOM_ROM_TARGET_PACKAGE) | cut -d ' ' -f 5`"
+	@echo "zip: "$(CUSTOM_ROM_TARGET_PACKAGE)
+	@echo "size:`ls -lah $(CUSTOM_ROM_TARGET_PACKAGE) | cut -d ' ' -f 5`"
 
 signed: target-files-package otatools
 	$(hide) $(HOST_OUT)/bin/sign_target_files_apks -o -d vendor/keys $(BUILT_TARGET_FILES_PACKAGE) $(SIGNED_TARGET_FILES_PACKAGE)
 	$(hide) $(HOST_OUT)/bin/ota_from_target_files -k vendor/keys/releasekey --block $(SIGNED_TARGET_FILES_PACKAGE) $(CUSTOM_ROM_TARGET_PACKAGE)
 	$(hide) rm $(BUILT_TARGET_FILES_PACKAGE)
-	@echo -e "signed zip: "$(CUSTOM_ROM_TARGET_PACKAGE)
-	@echo -e "size:`ls -lah $(CUSTOM_ROM_TARGET_PACKAGE) | cut -d ' ' -f 5`"
+	@echo "signed zip: "$(CUSTOM_ROM_TARGET_PACKAGE)
+	@echo "size:`ls -lah $(CUSTOM_ROM_TARGET_PACKAGE) | cut -d ' ' -f 5`"
